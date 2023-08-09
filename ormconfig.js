55a7cb4705e7ede54e09d1557bc5a8ff32a2ac42
Jang Yong Ho <96761280+JangKroed@users.noreply.github.com>
2023-08-09 12:54:08 +0900

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -2,13 +2,15 @@ require('dotenv/config')
 /* eslint @typescript-eslint/no-var-requires: 0 */
 const { SnakeNamingStrategy } = require('typeorm-naming-strategies')
 
+const { DB_NAME, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD } = process.env
+
 module.exports = {
   type: 'postgres',
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  database: DB_NAME,
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
   synchronize: false,
   autoLoadEntities: true,
   keepConnectionAlive: true,
